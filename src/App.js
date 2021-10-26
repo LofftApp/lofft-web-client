33abbc97ca49b8ca9806cb910c69dfab96c30754
James Hibbeard <jhibbeard@gmail.com>
2021-10-26 16:40:54 +0200

    Added process to retrieve data from the API, the link has been created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styling/App.scss";
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
+import EmailList from "./components/EmailList";
 
 function App() {
   return (
     <div className="App wrapper">
       <Header />
       <Welcome />
+      <EmailList />
     </div>
   );
 }
---
+++ src/components/EmailList.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+const EmailList = () => {
+  console.log("The email list has been imported");
+  const [emails, setEmails] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/api/v1/emails")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmails(data);
+      });
+  }, []);
+
+  return (
+    <ul>
+      {emails.map((email) => (
+        <li>{email}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default EmailList;
